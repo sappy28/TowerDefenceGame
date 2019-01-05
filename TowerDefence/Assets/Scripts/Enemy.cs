a86b325454b629608d361ea024e1da6692d63500
sappy28 <sparshbhardwaj418@gmail.com>
2019-01-05 12:46:29 +0530

    TD02

---
+++ TowerDefence/Assets/Scripts/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    
+    [SerializeField] private Transform exitPoint;
+    [SerializeField] private Transform[] waypoints;
+    [SerializeField] private float navigationUpdate;
+
+    private int target = 0 ;
+    private Transform enemy;
+    private float navigationTime = 0;
+
+	// Use this for initialization
+	void Start () {
+	    enemy = GetComponent<Transform> ();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( waypoints!= null)
+        {
+            navigationTime += Time.deltaTime;
+            if (navigationTime > navigationUpdate)
+            {
+                if(target < waypoints.Length)
+                    enemy.position = Vector2.MoveTowards(enemy.position , waypoints[target].position , navigationTime);
+                else 
+                    enemy.position = Vector2.MoveTowards(enemy.position , exitPoint.position , navigationTime);
+
+                navigationTime = 0;
+            }
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Checkpoint")
+        {
+            target++;
+        }
+        else if (other.tag == "Finish")
+        {
+            GameManager.Instance.removeEnemyOnScreen();
+            Destroy(gameObject);
+        }
+    }
+
+    
+}
